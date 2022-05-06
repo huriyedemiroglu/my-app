0126bdaac361471518ca9cd24172140346b16395
huriyedemiroglu <demirogluhu@gmail.com>
2022-05-06 11:33:21 -0700

    added

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.scss"
+
+
+
+const Header = () => {
+  return (
+    <div className="header-title">
+
+      <h1>POPULAR TOUR PLACES</h1>
+    </div>
+  )
+}
+
+export default Header;
